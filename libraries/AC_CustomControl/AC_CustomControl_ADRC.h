d23769c5ad6b4d27aebbfa5f7a41070df6a7927f
huangyangl <huangyangly@gmail.com>
2023-12-20 10:27:19 +0800

    adrc wait to build

---
+++ libraries/AC_CustomControl/AC_CustomControl_ADRC.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "AC_CustomControl_Backend.h"
+
+#ifndef CUSTOMCONTROL_ADRC_ENABLED
+    #define CUSTOMCONTROL_ADRC_ENABLED AP_CUSTOMCONTROL_ENABLED
+#endif
+
+#if CUSTOMCONTROL_ADRC_ENABLED
+
+class AC_CustomControl_ADRC : public AC_CustomControl_Backend {
+public:
+    AC_CustomControl_ADRC(AC_CustomControl& frontend, AP_AHRS_View*& ahrs, AC_AttitudeControl*& att_control, AP_MotorsMulticopter*& motors, float dt);
+
+
+    Vector3f update(void) override;
+    void reset(void) override;
+
+    // user settable parameters
+    static const struct AP_Param::GroupInfo var_info[];
+
+protected:
+    // declare parameters here
+    AC_ADRC _rate_roll_cont;
+    AC_ADRC _rate_pitch_cont;
+    AC_ADRC _rate_yaw_cont;
+};
+
+#endif
